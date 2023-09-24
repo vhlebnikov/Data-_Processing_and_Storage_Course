a3c55d4d31ecb9fa511b587360e55eba470a486f
vhlebnikov <v.khlebnikov@g.nsu.ru>
2023-09-24 19:13:50 +0700

    Copy from 7th task

---
+++ JavaConcurrency/Task_8/src/main/java/ru/nsu/khlebnikov/Worker.java
@@ -0,0 +1,34 @@
+package ru.nsu.khlebnikov;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Class for calculating partial sum of the Leibniz sequence.
+ */
+public class Worker implements Callable<Double> {
+    private final int from;
+    private final int to;
+
+    public Worker(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    /**
+     * Calculate partial sum of the Leibniz sequence from "from" inclusive to "to" exclusive.
+     *
+     * @return partial sum.
+     */
+    @Override
+    public Double call() {
+        double result = 0;
+        for (double i = from; i < to; i++) {
+            if (i % 2 == 0) {
+                result += 1 / (2 * i + 1);
+            } else {
+                result -= 1 / (2 * i + 1);
+            }
+        }
+        return result * 4;
+    }
+}
---
+++ JavaConcurrency/Task_8/src/main/java/ru/nsu/khlebnikov/WorkersFactory.java
@@ -0,0 +1,57 @@
+package ru.nsu.khlebnikov;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Class that represents factory for distribution work of calculating partial sums
+ * between provided thread and obtaining the final result (pi value).
+ */
+public class WorkersFactory {
+    private final ExecutorService executorService;
+    List<Worker> workers = new ArrayList<>();
+    List<Future<Double>> workersResults = new ArrayList<>();
+    private final int numberOfIterations;
+    private final int numberOfThreads;
+
+    public WorkersFactory(int numberOfThreads, int numberOfIterations) {
+        executorService = Executors.newFixedThreadPool(numberOfThreads);
+        this.numberOfIterations = numberOfIterations;
+        this.numberOfThreads = numberOfThreads;
+    }
+
+    /**
+     * Calculate pi value for "countOfIterations" iterations.
+     *
+     * @return pi value.
+     * @throws InterruptedException if interrupted exception occurred
+     */
+    public double calculate() throws InterruptedException {
+        int div = numberOfIterations / numberOfThreads;
+        int mod = numberOfIterations % numberOfThreads;
+
+        int start = 0;
+        int end;
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            end = mod-- > 0 ? start + div + 1 : start + div;
+            workers.add(new Worker(start, end));
+            start = end;
+        }
+
+        workersResults = executorService.invokeAll(workers);
+        executorService.shutdown();
+
+        return workersResults.stream().map(x -> {
+            try {
+                return x.get();
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }).reduce(0d, Double::sum);
+    }
+}
