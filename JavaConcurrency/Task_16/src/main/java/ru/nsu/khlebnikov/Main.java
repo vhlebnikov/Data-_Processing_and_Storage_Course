0541b1b36912859a51a1f26a3c0a748cc988debe
Вадим <v.khlebnikov@g.nsu.ru>
2023-10-31 13:16:18 +0700

    Made changes

--- JavaConcurrency/Task_16/src/main/java/ru/nsu/khlebnikov/Main.java
+++ JavaConcurrency/Task_16/src/main/java/ru/nsu/khlebnikov/Main.java
@@ -9,36 +9,58 @@ import java.util.Scanner;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
+    private static HttpURLConnection connection;
+    private static Scanner scanner;
+    private static Thread readerThread;
+    private static BufferedReader reader;
     public static LinkedBlockingQueue<String> lines = new LinkedBlockingQueue<>();
+    private static final int LINES_IN_BLOCK = 25;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner scanner = new Scanner(System.in);
-        URL url = new URL(scanner.nextLine());
+    public static void main(String[] args) {
+        try {
+            try {
+                scanner = new Scanner(System.in);
+                URL url = new URL(scanner.nextLine());
 
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        new Thread(new Reader(reader, lines)).start();
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                readerThread = new Thread(new Reader(reader, lines));
+                readerThread.start();
 
-        Thread.sleep(1000);
-
-        int iterations = 0;
-        while (!lines.isEmpty()) {
-            if (iterations == 25) {
-                System.out.println("Press space to scroll down");
-                iterations++;
-            } else if (iterations > 25) {
-                if (scanner.nextLine().equals(" ")) {
-                    iterations = 0;
+                int iterations = 0;
+                while (!lines.isEmpty()) {
+                    if (iterations == LINES_IN_BLOCK) {
+                        System.out.println("Press space to scroll down");
+                        iterations++;
+                    } else if (iterations > LINES_IN_BLOCK) {
+                        if (scanner.nextLine().equals(" ")) {
+                            iterations = 0;
+                        }
+                    } else {
+                        System.out.println(lines.poll());
+                        iterations++;
+                    }
                 }
-            } else {
-                System.out.println(lines.poll());
-                iterations++;
+            } finally {
+                if (readerThread != null) {
+                    readerThread.interrupt();
+                }
+                if (reader != null) {
+                    reader.close();
+                }
+                if (scanner != null) {
+                    scanner.close();
+                }
+                if (connection != null) {
+                    connection.disconnect();
+                }
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
     }
 }
 
